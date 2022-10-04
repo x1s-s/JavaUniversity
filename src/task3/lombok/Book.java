142c7aa578f1ab366c0de3894fc13ef10125076e
Aliaksei Shkurko <shaleprog@gmail.com>
2022-10-04 13:33:45 +0300

    add lombok annotation to Book.java

--- src/task3/lombok/Book.java
+++ src/task3/lombok/Book.java
@@ -1,8 +1,14 @@
 package task3.lombok;
 
+import lombok.*;
+
 import java.util.Date;
 import java.util.Scanner;
-
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Getter
+@Setter
 public class Book implements InputOutput {
   private Author author;
   private Genre genre;
@@ -10,62 +16,9 @@ public class Book implements InputOutput {
   private String name;
   private Date creationDate;
 
-  public Book(Author author, Genre genre, PublishingHouse publishingHouse, String name,
-              Date creationDate) {
-    this.author = author;
-    this.genre = genre;
-    this.publishingHouse = publishingHouse;
-    this.name = name;
-    this.creationDate = creationDate;
-  }
-
-  public Book(){
-    this(new Author(), Genre.FANTASY,new PublishingHouse(),"",new Date());
-  }
-
   public Book(Scanner scanner){
     this();
     this.fill(scanner);
-  }
-
-  public Author getAuthor() {
-    return author;
-  }
-
-  public void setAuthor(Author author) {
-    this.author = author;
-  }
-
-  public Genre getGenre() {
-    return genre;
-  }
-
-  public void setGenre(Genre genre) {
-    this.genre = genre;
-  }
-
-  public PublishingHouse getPublishingHouse() {
-    return publishingHouse;
-  }
-
-  public void setPublishingHouse(PublishingHouse publishingHouse) {
-    this.publishingHouse = publishingHouse;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public Date getCreationDate() {
-    return creationDate;
-  }
-
-  public void setCreationDate(Date creationDate) {
-    this.creationDate = creationDate;
   }
 
   @Override
@@ -87,14 +40,4 @@ public class Book implements InputOutput {
     creationDate = new Date(scanner.nextInt());
   }
 
-  @Override
-  public String toString() {
-    return "Book{" +
-        "author=" + author +
-        ", genre=" + genre +
-        ", publishingHouse=" + publishingHouse +
-        ", name='" + name + '\'' +
-        ", creationDate=" + creationDate +
-        '}';
-  }
 }
